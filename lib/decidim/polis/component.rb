5dad9dbee67c1dc77282b3ee498538a6c9a6bb91
Armand <fardeauarmand@gmail.com>
2019-09-04 11:48:44 +0200

    Use component instead of feature

--- lib/decidim/polis/component.rb
+++ lib/decidim/polis/component.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require_dependency "decidim/features/namer"
+require_dependency "decidim/components/namer"
 
-Decidim.register_feature(:polis) do |feature|
-  feature.engine = Decidim::Polis::Engine
-  feature.admin_engine = Decidim::Polis::AdminEngine
-  feature.icon = "decidim/polis/icon.svg"
-  feature.stylesheet = "decidim/polis/polis"
+Decidim.register_component(:polis) do |component|
+  component.engine = Decidim::Polis::Engine
+  component.admin_engine = Decidim::Polis::AdminEngine
+  component.icon = "decidim/polis/icon.svg"
+  component.stylesheet = "decidim/polis/polis"
 
-  feature.settings(:global) do |settings|
+  component.settings(:global) do |settings|
     settings.attribute :title, type: :text, translated: true
     settings.attribute :description, type: :text, editor: true, translated: true
     settings.attribute :sign_in_social, type: :boolean, default: false
     settings.attribute :visualization, type: :boolean, default: false
   end
 
-  feature.settings(:step) do |settings|
+  component.settings(:step) do |settings|
     settings.attribute :user_can_write, type: :boolean, default: false
     settings.attribute :user_can_vote, type: :boolean, default: false
   end
