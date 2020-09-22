7b4f5983ee77c87e6a2004278cc6f69073142179
dynnammo <baptiste.thivend@insa-lyon.fr>
2020-09-22 14:18:32 +0200

    Add tests for polis page

--- lib/decidim/polis/test/factories.rb
+++ lib/decidim/polis/test/factories.rb
@@ -8,6 +8,49 @@ FactoryBot.define do
     manifest_name { :polis }
     participatory_space { create(:participatory_process, :with_steps) }
   end
-
   # Add engine factories here
+end
+
+FactoryBot.modify do
+  factory :organization, class: "Decidim::Organization" do
+    name { Faker::Company.unique.name }
+    reference_prefix { Faker::Name.suffix }
+    time_zone { "UTC" }
+    twitter_handler { Faker::Hipster.word }
+    facebook_handler { Faker::Hipster.word }
+    instagram_handler { Faker::Hipster.word }
+    youtube_handler { Faker::Hipster.word }
+    github_handler { Faker::Hipster.word }
+    sequence(:host) { |n| "#{n}.lvh.me" }
+    description { Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title } }
+    favicon { Decidim::Dev.test_file("icon.png", "image/png") }
+    default_locale { Decidim.default_locale }
+    available_locales { Decidim.available_locales }
+    users_registration_mode { :enabled }
+    official_img_header { Decidim::Dev.test_file("avatar.jpg", "image/jpeg") }
+    official_img_footer { Decidim::Dev.test_file("avatar.jpg", "image/jpeg") }
+    official_url { Faker::Internet.url }
+    highlighted_content_banner_enabled { false }
+    enable_omnipresent_banner { false }
+    badges_enabled { true }
+    user_groups_enabled { true }
+    send_welcome_notification { true }
+    admin_terms_of_use_body { Decidim::Faker::Localized.wrapped("<p>", "</p>") { generate_localized_title } }
+    force_users_to_authenticate_before_access_organization { false }
+    smtp_settings do
+      {
+          "from" => "test@example.org",
+          "user_name" => "test",
+          "encrypted_password" => Decidim::AttributeEncryptor.encrypt("demo"),
+          "port" => "25",
+          "address" => "smtp.example.org"
+      }
+    end
+    polis_site_id { Faker::Hipster.word }
+
+    after(:create) do |organization|
+      tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization)
+      create(:static_page, :tos, organization: organization) if tos_page.nil?
+    end
+  end
 end
---
+++ spec/system/polis_spec.rb
@@ -0,0 +1,59 @@
+
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe "pol.is", type: :system do
+  include_context "with a component"
+
+  let(:site_id) { "${{ secrets.POLIS_SITE_ID }}" } # to store in local
+  let(:organization) { create(:organization, polis_site_id: site_id) }
+  let(:user) { create :user, :confirmed, organization: organization }
+  # let(:step) { participatory_process.steps.first }
+  let(:manifest_name) { "polis" }
+
+
+  before do
+    switch_to_host(organization.host)
+    visit_component
+  end
+
+  it "should always display the pol.is iframe" do
+    within ".polis" do
+      expect(page).to have_css("iframe")
+    end
+  end
+
+  context "when user isn't logged in" do
+    it 'should encourage to log in' do
+      expect(page).to have_content("Login with your account")
+    end
+  end
+
+  context "when user is logged in" do
+    before do
+      login_as user, scope: :user
+      visit_component
+    end
+
+    it 'should not display comments and participants' do
+      expect(page).not_to have_content("Comments")
+      expect(page).not_to have_content("Participants")
+    end
+  end
+
+  context "when user can manage decidim" do
+    let(:user) { create :user, :admin, :confirmed, organization: organization }
+
+    before do
+      login_as user, scope: :user
+      visit_component
+    end
+
+    it 'should display the admin section' do
+      expect(page).to have_content("Admin Pol.is")
+      expect(page).to have_content("Comments")
+      expect(page).to have_content("Participants")
+    end
+  end
+end
